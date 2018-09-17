79f694620e8890d0e0253982189139be5b4dec28
Smrutirekha Khatua <smrutirekha.khatua@gmail.com>
2018-09-17 23:52:20 +0530

    Added

---
+++ src/main/java/demo/practice/honeywell/SecondLargestNumber.java
@@ -0,0 +1,21 @@
+package demo.practice.honeywell;
+
+public class SecondLargestNumber {
+
+    public static void main(String args[]){
+        int arr[] = {2,20,5,3,8,30,21,50,42,31};
+        int largest = Integer.MIN_VALUE;
+        int secondlargest = largest ;
+        for(int i =0;i<arr.length;i++){
+
+            if(arr[i]>largest){
+                secondlargest = largest;
+                largest = arr[i];
+            }else if(arr[i]>secondlargest && arr[i]!=largest){
+                secondlargest = arr[i];
+            }
+        }
+
+        System.out.println(largest+"  "+secondlargest);
+    }
+}
